0ff989e68943975608631ed7c717be22da30d3a8
Félix Chénier <chenier.felix@uqam.ca>
2021-11-08 09:11:33 -0500

    Launchers for mac and windows

--- launchers/windows/install_mosa.bat
+++
@@ -1,9 +0,0 @@
-call %HOMEPATH%\miniconda3\Scripts\activate.bat
-call conda create -y -n mosa -c conda-forge python=3.8 git pip limitedinteraction
-call %HOMEPATH%\miniconda3\Scripts\deactivate.bat
-
-call %HOMEPATH%\miniconda3\Scripts\activate.bat mosa
-pip install git+https://github.com/felixchenier/mosa
-python -c "import mosa; mosa.install()"
-
-python -c "import limitedinteraction as li; li.button_dialog('Installation terminée.', ['OK'])"
--- launchers/windows/mosa.bat
+++
@@ -1,2 +0,0 @@
-call %HOMEPATH%\miniconda3\Scripts\activate.bat mosa
-python -c "import mosa; mosa.start_menu()"
